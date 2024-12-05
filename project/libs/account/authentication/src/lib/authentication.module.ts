9724e245cb16fcdf3b42c6c41f7d36105afb1942
Rostislav <rabelousov@gmail.com>
2024-12-06 00:42:35 +0300

    Добавляет AuthenticationController и AuthenticationService

--- project/libs/account/authentication/src/lib/authentication.module.ts
+++ project/libs/account/authentication/src/lib/authentication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationController } from './authentication.controller';
+import { AuthenticationService } from './authentication.service';
 
 @Module({
-  controllers: [],
-  providers: [],
+  controllers: [AuthenticationController],
+  providers: [AuthenticationService],
   exports: [],
 })
 export class AuthenticationModule {}
