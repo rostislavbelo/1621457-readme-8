d433bbcfa8bb3c49660e5d402c41ea6501371180
Rostislav <rabelousov@gmail.com>
2024-12-06 01:14:27 +0300

    Добавляет интерфейсы EntityFactory и EntityFactory

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -1,3 +1,5 @@
 export { User } from './lib/types/user.interface';
 export { AuthUser } from './lib/types/auth-user.interface';
 export { Entity } from './lib/base/entity';
+export { StorableEntity } from './lib/interfaces/storable-entity.interface';
+export { EntityFactory } from './lib/interfaces/entity-factory.interface';
