911618e90a96b460afa4c2008bc518cca56daa2a
Rostislav <rabelousov@gmail.com>
2024-12-07 01:05:05 +0300

    Имплементируем BlogUserRepository

--- project/libs/account/blog-user/src/index.ts
+++ project/libs/account/blog-user/src/index.ts
@@ -1,3 +1,3 @@
 export { BlogUserModule } from './blog-user-module/blog-user.module';
-export { BlogUserFactory } from './blog-user-module/blog-user.factory';
 export { BlogUserEntity } from './blog-user-module/blog-user.entity';
+export { BlogUserRepository } from './blog-user-module/blog-user.repository';
