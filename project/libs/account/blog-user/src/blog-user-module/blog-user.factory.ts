3f50e950042feba2112ac05b67e3c33ba5484aa2
Rostislav <rabelousov@gmail.com>
2024-12-07 01:34:37 +0300

    Ррегистрирует BlogUserRepository и BlogUserFactory как @Injectable

--- project/libs/account/blog-user/src/blog-user-module/blog-user.factory.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.factory.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { AuthUser, EntityFactory } from '@project/core';
 import { BlogUserEntity } from './blog-user.entity';
 
+@Injectable()
 export class BlogUserFactory implements EntityFactory<BlogUserEntity> {
   public create(entityPlainData: AuthUser): BlogUserEntity {
     return new BlogUserEntity(entityPlainData);
--- project/libs/shared/data-access/src/index.ts
+++ project/libs/shared/data-access/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/data-access.module';
+export * from './repository/base-memory.repository';
