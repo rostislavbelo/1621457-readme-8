5733bb0688e2f83c94f8b4330c12021eec3db051
Rostislav <rabelousov@gmail.com>
2024-12-08 19:34:34 +0300

    Добавляет импроты BlogUserModule, AuthenticationModule

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { BlogUserModule } from '@project/blog-user';
+import { AuthenticationModule } from '@project/authentication';
 
 @Module({
-  imports: [],
+  imports: [
+    BlogUserModule,
+    AuthenticationModule
+  ],
   controllers: [],
   providers: [],
 })
