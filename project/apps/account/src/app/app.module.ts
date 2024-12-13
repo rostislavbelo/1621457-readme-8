41981e69791349f1c6675ad9523be54d69718903
Rostislav <rabelousov@gmail.com>
2024-12-14 01:42:30 +0300

    Подключит MongooseModule в корневой модуль сервиса account

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BlogUserModule } from '@project/blog-user';
 import { AuthenticationModule } from '@project/authentication';
-import { AccountConfigModule } from '@project/account-config';
+import { AccountConfigModule, getMongooseOptions } from '@project/account-config';
 
 @Module({
   imports: [
     BlogUserModule,
     AuthenticationModule,
     AccountConfigModule,
+    MongooseModule.forRootAsync(
+      getMongooseOptions()
+    )
   ],
   controllers: [],
   providers: [],
