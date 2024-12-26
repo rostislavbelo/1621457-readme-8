697bfd30523d757076b8800c9c59359d52ab6793
Rostislav <rabelousov@gmail.com>
2024-12-26 15:05:59 +0300

    Убирает ConfigService из \project\apps\blog\src\main.ts

--- project/apps/blog/src/main.ts
+++ project/apps/blog/src/main.ts
@@ -1,18 +1,28 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-const DEFAULT_PORT = 3000;
 const GLOBAL_PREFIX = 'api';
+const DEFAULT_PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('The «Blog» service')
+  .setDescription('Blog service API')
+  .setVersion('1.0')
+  .build();
+
   app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
   const port = process.env.PORT || DEFAULT_PORT;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`);
 }
 
 bootstrap();
