ace691f604745463aa64efd8e177689c49d2b5b2
Rostislav <rabelousov@gmail.com>
2025-01-17 20:25:57 +0300

    Добавит базовую валидацию для сервиса account, убирает дату регистрации юзера из create-user.dto

--- project/libs/account/authentication/src/dto/create-user.dto.ts
+++ project/libs/account/authentication/src/dto/create-user.dto.ts
@@ -1,33 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, IsUrl, Length } from 'class-validator';
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
 
 export class CreateUserDto {
     @ApiProperty({
         description: 'User unique address',
         example: 'user@user.ru'
       })
+    @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })  
     public email: string;
 
     @ApiProperty({
         description: 'User name',
         example: 'Keks',
       })
+    @IsString()
+    @Length(3, 50)  
     public name: string;
 
     @ApiProperty({
-        description: 'Registration date',
-        example: '1981-03-12',
-      })
-    public registrationDate: Date;
-
-    @ApiProperty({
-        description: 'Avatar Id',
-        example: 'any random text',
-      })    
-    avatarId?: string;
+      description: 'User avatar path',
+      example: '/images/user.png',
+      required: false,
+    })
+    @IsUrl()
+    public avatar?: string;
 
     @ApiProperty({
         description: 'Password',
         example: '123456',
-      })    
+      })
+    @IsString()
+    @Length(6, 12)     
     password: string;
 }
