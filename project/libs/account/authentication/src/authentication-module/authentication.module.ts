378b34c81fd1762ddde6744f0088f49e82172c0d
Rostislav <rabelousov@gmail.com>
2025-01-20 20:15:20 +0300

    Добавляет jwt в authentication.module.ts

--- project/libs/account/authentication/src/authentication-module/authentication.module.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 import { BlogUserModule } from '@project/blog-user';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { getJwtOptions } from '@project/account-config';
+import { JwtAccessStrategy } from '../strategies/jwt-access.strategy';
 
 @Module({
-  imports: [BlogUserModule],
+  imports: [
+    BlogUserModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: getJwtOptions,
+    }),
+  ],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [AuthenticationService, JwtAccessStrategy],
 })
 export class AuthenticationModule {}
