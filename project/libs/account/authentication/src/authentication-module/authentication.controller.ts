e8c54bd3f72cd265a9967a99d0bcc33776b17c21
Rostislav <rabelousov@gmail.com>
2025-01-20 16:59:35 +0300

    Добавляет authentication.controller.ts

--- project/libs/account/authentication/src/authentication-module/authentication.controller.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.controller.ts
@@ -6,6 +6,7 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LoggedUserRdo } from '../rdo/logged-user.rdo';
 import { UserRdo } from '../rdo/user.rdo';
 import { AuthenticationResponseMessage } from './authentication.constant';
+import { MongoIdValidationPipe } from '@project/pipes';
 
 @ApiTags('authentication')
 @Controller('auth')
@@ -53,7 +54,7 @@ export class AuthenticationController {
       description: AuthenticationResponseMessage.UserNotFound,
     })  
     @Get(':id')
-    public async show(@Param('id') id: string) {
+    public async show(@Param('id', MongoIdValidationPipe) id: string) {
       const existUser = await this.authService.getUser(id);
       return existUser.toPOJO();
     }
